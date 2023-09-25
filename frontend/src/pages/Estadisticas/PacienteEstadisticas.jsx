26a03fb5c076d0ad913449317904df6f032a2e85
federico-pena <federpena22@gmail.com>
2023-09-24 21:52:53 -0300

    estadisticas

--- frontend/src/pages/Estadisticas/PacienteEstadisticas.jsx
+++ frontend/src/pages/Estadisticas/PacienteEstadisticas.jsx
@@ -6,36 +6,34 @@ export const PacienteEstadisticas = ({
 	tratamientosPacientes,
 }) => {
 	return (
-		estadisticasPacientes && (
-			<section className='pacientesDatos'>
-				<h2>Datos de Pacientes</h2>
-				<ul className='ulPacientesDatos'>
-					<li>
-						Pacientes
-						<strong>
-							<TransitionNumber
-								from={0}
-								to={estadisticasPacientes.totalPacientes}
-								duration={2000}
-							/>
-						</strong>
-					</li>
-					<li>
-						Promedio De Edad
-						<strong>
-							<TransitionNumber
-								from={0}
-								to={estadisticasPacientes.promedioDeEdades}
-								duration={2000}
-							/>{' '}
-							<span> años</span>
-						</strong>
-					</li>
-				</ul>
-				<section className='pacientesPorcentaje'>
-					<PorcentajesComponent datos={tratamientosPacientes} />
-				</section>
+		<section className='pacientesDatos'>
+			<h2>Datos de Pacientes</h2>
+			<ul className='ulPacientesDatos'>
+				<li>
+					Pacientes
+					<strong>
+						<TransitionNumber
+							from={0}
+							to={estadisticasPacientes.totalPacientes}
+							duration={2000}
+						/>
+					</strong>
+				</li>
+				<li>
+					Promedio De Edad
+					<strong>
+						<TransitionNumber
+							from={0}
+							to={estadisticasPacientes.promedioDeEdades}
+							duration={2000}
+						/>{' '}
+						<span> años</span>
+					</strong>
+				</li>
+			</ul>
+			<section className='pacientesPorcentaje'>
+				<PorcentajesComponent datos={tratamientosPacientes} />
 			</section>
-		)
+		</section>
 	)
 }
